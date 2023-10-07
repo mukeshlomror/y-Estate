8a38b414bd44f7db5d3728ccc9a7818013ba95f3
MukeshLomror <mukeshlomror15@gmail.com>
2023-10-07 05:26:00 -0700

    installed reactjs and tailwind css for basic project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <h1 className='text-3xl text-red-400 font-bold underline bg-white'>I am mukesh lomror</h1>
+    </>
+  )
+}
+
+export default App
